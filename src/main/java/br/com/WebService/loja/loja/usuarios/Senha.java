dd6607f3a038458b78570afcb6997047b808227c
Gabriel Longatti <galongatti@hotmail.com>
2020-11-22 12:02:25 -0300

    Adição de restrição dpara operação de post apenas para role admin

--- src/main/java/br/com/WebService/loja/loja/usuarios/Senha.java
+++ src/main/java/br/com/WebService/loja/loja/usuarios/Senha.java
@@ -1,20 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.WebService.loja.loja.usuarios;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-/**
- *
- * @author GaLon
- */
-public class Senha {
-
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("123"));
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package br.com.WebService.loja.loja.usuarios;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+///**
+// *
+// * @author GaLon
+// */
+//public class Senha {
+//
+//    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        System.out.println(encoder.encode("123"));
+//    }
+//}
